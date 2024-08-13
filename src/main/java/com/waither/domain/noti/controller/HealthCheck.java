9057b2f6a1d4529bc26d7697da06ca1ab80ec76f
Kim Dong Hyun <98632435+DDonghyeo@users.noreply.github.com>
2024-08-14 05:59:07 +0900

    ♻️refactor : Update HealthCheck

--- src/main/java/com/waither/domain/noti/controller/HealthCheck.java
+++ src/main/java/com/waither/domain/noti/controller/HealthCheck.java
@@ -1,6 +1,6 @@
 package com.waither.domain.noti.controller;
 
-import com.waither.global.response.ApiResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
