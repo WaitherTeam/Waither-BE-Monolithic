7ffe21ea90e1351dbc15f96cea7a473b0f448616
DDonghyeo <gsu9045000@gmail.com>
2024-08-13 19:21:00 +0900

    ♻️refactor : LocationDto Validation 수정

--- src/main/java/com/waither/domain/noti/dto/request/LocationDto.java
+++ src/main/java/com/waither/domain/noti/dto/request/LocationDto.java
@@ -4,15 +4,16 @@ import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
+import lombok.NonNull;
 
 @Builder
 public record LocationDto (
-        @NotBlank(message = " 위도(latitude) 값은 필수입니다.")
+        @NonNull
         @DecimalMax(value = "43.0", inclusive = true, message = "대한민국 내에서만 가능합니다. (33~43)")
         @DecimalMin(value = "33.0", inclusive = true, message = "대한민국 내에서만 가능합니다. (33~43)")
         double latitude,
 
-        @NotBlank(message = " 경도(longitude) 값은 필수입니다.")
+        @NonNull
         @DecimalMax(value = "132.0", inclusive = true, message = "대한민국 내에서만 가능합니다. (124~132)")
         @DecimalMin(value = "124.0", inclusive = true, message = "대한민국 내에서만 가능합니다. (124~132)")
         double longitude
