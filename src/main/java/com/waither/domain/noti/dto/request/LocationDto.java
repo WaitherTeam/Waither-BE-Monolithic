9937c402f393b68905cd2a8df338559478001b14
DDonghyeo <gsu9045000@gmail.com>
2024-08-21 16:29:19 +0900

    ♻️refactor : Dto 스펙 수정

--- src/main/java/com/waither/domain/noti/dto/request/LocationDto.java
+++ src/main/java/com/waither/domain/noti/dto/request/LocationDto.java
@@ -11,12 +11,12 @@ public record LocationDto (
         @NonNull
         @DecimalMax(value = "43.0", inclusive = true, message = "대한민국 내에서만 가능합니다. (33~43)")
         @DecimalMin(value = "33.0", inclusive = true, message = "대한민국 내에서만 가능합니다. (33~43)")
-        double latitude,
+        Double latitude,
 
         @NonNull
         @DecimalMax(value = "132.0", inclusive = true, message = "대한민국 내에서만 가능합니다. (124~132)")
         @DecimalMin(value = "124.0", inclusive = true, message = "대한민국 내에서만 가능합니다. (124~132)")
-        double longitude
+        Double longitude
 ) {
 
 
