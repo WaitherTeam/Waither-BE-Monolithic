f9652f2c79fcfa10a63dc4816f5646416bb77e35
seheonnn <ho78901@naver.com>
2024-09-08 18:23:46 +0900

    refactor: DailyWeather 시간 변환 로직 수정 및 gitignore 추가

--- src/main/java/com/waither/domain/weather/service/WeatherService.java
+++ src/main/java/com/waither/domain/weather/service/WeatherService.java
@@ -169,15 +169,18 @@ public class WeatherService {
 
 		String expectedWeatherKey = regionName + "_" + convertLocalDateTimeToString(now);
 
-		LocalDateTime dailyWeatherBaseTime = convertLocalDateTimeToDailyWeatherTime(now.minusHours(1));
-		String dailyWeatherKey = regionName + "_" + convertLocalDateTimeToString(dailyWeatherBaseTime);
+		LocalDateTime dailyWeatherBaseTime = convertLocalDateTimeToDailyWeatherTime(now);
+		String dailyWeatherKey = regionName + "_" + convertLocalDateTimeToString(dailyWeatherBaseTime.plusHours(1));
+
+		log.info("[Main - api] dailyWeatherKey : {}", dailyWeatherKey);
+		log.info("[Main - api] expectedWeatherKey : {}", expectedWeatherKey);
 
 		DailyWeather dailyWeather = dailyWeatherRepository.findById(dailyWeatherKey)
 			.orElseGet(() -> {
 				log.info("[Main - api] Daily Weather Error");
 				try {
-					log.info(String.valueOf(now.minusHours(3)));
-					String[] baseTime = convertLocalDateTimeToString(now.minusHours(3)).split("_");
+					LocalDateTime dailyTime = convertLocalDateTimeToDailyWeatherTime(now);
+					String[] baseTime = convertLocalDateTimeToString(dailyTime).split("_");
 					createDailyWeather(region.getStartX(), region.getStartY(), baseTime[0], baseTime[1]);
 				} catch (URISyntaxException e) {
 					throw new CustomException(WeatherErrorCode.WEATHER_URI_ERROR);
@@ -232,13 +235,13 @@ public class WeatherService {
 	public LocalDateTime convertLocalDateTimeToDailyWeatherTime(LocalDateTime time) {
 
 		// DailyWeather 정보는 3시간마다
-		List<Integer> scheduledHours = Arrays.asList(0, 3, 6, 9, 12, 15, 18, 21);
+		List<Integer> scheduledHours = Arrays.asList(2, 5, 8, 11, 14, 17, 20, 23);
 
 		int currentHour = time.getHour();
 		int adjustedHour = scheduledHours.stream()
 			.filter(hour -> hour <= currentHour)
 			.reduce((first, second) -> second)
-			.orElse(scheduledHours.get(scheduledHours.size() - 1)); // 이전 날의 마지막 스케줄 시간(21시) 반환
+			.orElse(scheduledHours.get(scheduledHours.size() - 1));
 
 		// 현재 시간이 첫 스케줄 시간(0시)보다 작을 경우, 전날의 마지막 스케줄 시간으로 설정
 		if (currentHour < scheduledHours.get(0)) {
