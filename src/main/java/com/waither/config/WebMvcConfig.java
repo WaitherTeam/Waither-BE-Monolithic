219c0b999d5b200b6a1a5811708718d2e74c6b59
DDonghyeo <gsu9045000@gmail.com>
2024-11-06 18:24:48 +0900

    🐞bug : Swagger 페이지 렌더링 수정

--- src/main/java/com/waither/config/WebMvcConfig.java
+++ src/main/java/com/waither/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.waither.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -11,4 +12,14 @@ import java.util.List;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
+                .resourceChain(false);
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/")
+                .resourceChain(false);
+    }
 }
