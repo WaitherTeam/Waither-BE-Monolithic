5d2d0cb1e2e06ef3fa59e489f20ae3008e7eb029
Keemzino <69239293+jinho7@users.noreply.github.com>
2024-11-11 17:01:56 +0900

    🐛 fix: 경도,위도 test 스키마 수정

--- src/main/java/com/waither/domain/weather/dto/request/GetWeatherRequest.java
+++ src/main/java/com/waither/domain/weather/dto/request/GetWeatherRequest.java
@@ -9,7 +9,7 @@ public record GetWeatherRequest(
 	@NotNull(message = "[ERROR] 위도 입력은 필수 입니다.")
 	@DecimalMin(value = "33.0", message = "[ERROR] 위도는 33.0도에서 43.0도 사이여야 합니다.")
 	@DecimalMax(value = "43.0", message = "[ERROR] 위도는 33.0도에서 43.0도 사이여야 합니다.")
-	@Schema(description = "경도", example = "126.7340")
+	@Schema(description = "위도", example = "37.7151")
 	Double latitude,
 	@NotNull(message = "[ERROR] 경도 입력은 필수 입니다.")
 	@DecimalMin(value = "124.0", message = "[ERROR] 경도는 124.0도에서 132.0도 사이여야 합니다.")
